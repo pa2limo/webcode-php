33772eceae8ae375f15de0e27fe5c162d8524798
Muhammad Pudhail <pa2limo@users.noreply.github.com>
2014-07-21 04:01:07 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * A simple PHP MVC skeleton - Modified By Pa2limo 
+ *
+ * @package php-mvc
+ * @author Panique
+ * @link http://www.php-mvc.net
+ * @link https://github.com/panique/php-mvc/
+ * @license http://opensource.org/licenses/MIT MIT License
+ */
+
+// load the (optional) Composer auto-loader
+if (file_exists('vendor/autoload.php')) {
+    require 'vendor/autoload.php';
+}
+
+// load application config (error reporting etc.)
+require 'application/config/config.php';
+
+// load application class
+require 'application/libs/application.php';
+require 'application/libs/controller.php';
+require 'application/libs/classpaging.php';
+require 'application/libs/mylib.php';
+// start the application
+$app = new Application();
