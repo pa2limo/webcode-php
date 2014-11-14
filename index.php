86934e76f3d23339b429c8d1eae8edb1d49366d4
Muhammad Pudhail <pa2limo@users.noreply.github.com>
2014-11-15 00:53:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,6 @@ require 'application/config/config.php';
 // load application class
 require 'application/libs/application.php';
 require 'application/libs/controller.php';
-require 'application/libs/classpaging.php';
 require 'application/libs/mylib.php';
 // start the application
 $app = new Application();
