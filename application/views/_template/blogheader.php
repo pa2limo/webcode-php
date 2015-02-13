33aae7328c5931168ece48baaa762fb6e1d1fc15
Muhammad Pudhail <pa2limo@users.noreply.github.com>
2015-02-13 13:59:42 +0700

    Update blogheader.php

--- application/views/_template/blogheader.php
+++ application/views/_template/blogheader.php
@@ -1,1 +1,31 @@
-// blog header
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>WEBCODE - Technical Post about Web and Coding PHP 5</title>
+	<meta name="viewport"    content="width=device-width, initial-scale=1.0">
+	<!--<meta name="viewport" 	 content="width=device-width,user-scalable=no"> disable user scaling -->
+	<!--<meta name="viewport" 	 content="width=320,height=480""> forced to fixed scaling -->
+	<meta name="description" content="Web Best Practices, PHP Best Practices, PHP Coding Guide, PHP Snippet,  PHP PDO, Bootstrap, Web Template">
+	<meta name="author"      content="Muhammad Pudhail">
+	<meta name="robots" 	 content="index">
+
+	<!--<meta http-equiv="refresh" content="10">  refresh 10s -->
+	<!--<meta http-equiv="refresh" content="10;url=http://redirect-url.com">  redirect after 10s -->
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,700,400italic">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css"> 
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
+	
+	<link href="<?php echo ASSET; ?>css/style.css" rel="stylesheet" />
+	<!-- Theme skin -->
+	<link href="<?php echo ASSET; ?>css/default.css" rel="stylesheet" />
+    
+	<style type="text/css">
+	</style> 
+	<!-- css -->
+	<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif] -->
+</head>
+
