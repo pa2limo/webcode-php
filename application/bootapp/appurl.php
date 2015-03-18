8059cc921011f124562d9e779bb0de991e55804d
Muhammad Pudhail <pa2limo@users.noreply.github.com>
2015-03-18 13:35:20 +0700

    Update appurl.php

--- application/bootapp/appurl.php
+++ application/bootapp/appurl.php
@@ -1,85 +1,61 @@
 <?php
 
 class Appurl {
-    /** @var null The controller */
+    /** @var all null  */
     private $url_controller = null;
-
-    /** @var null The method (of the above controller), often also named "action" */
     private $url_action = null;
+    private $url_params = array();
 
-    /** @var null Parameter one */
-    private $url_parameter_1 = null;
-
-    /** @var null Parameter two */
-    private $url_parameter_2 = null;
-
-    /** @var null Parameter three */
-    private $url_parameter_3 = null;
-
-    /**
-     * "Start" the application:
-     * Analyze the URL elements and calls the according controller/method or the fallback
-     */
+    /** "Boots Up" the application:  */
     public function __construct() {
-        // create array with URL parts in $url
-        $this->splitUrl();
-        // check for controller: does such a controller exist ?
-        if (file_exists('./application/controller/' . $this->url_controller . '.php')) {
-            // if so, then load this file and create this controller
-            // example: if controller would be "car", then this line would translate into: $this->car = new car();
-            
-            require './application/controller/' . $this->url_controller . '.php';
+        
+        $this->splitUrl(); // create array with URL parts in $url       
+        if (!$this->url_controller) { // check for controller: no controller given ? load home
+            require './application/controller/home.php';
+            $page = new Home();
+            $page->index(); 
+
+        }elseif (file_exists('./application/controller/'. $this->url_controller .'.php')) { // check for controller            
+            require './application/controller/' . $this->url_controller . '.php'; // load file, create controller
             $this->url_controller = new $this->url_controller();
-
-            // check for method: does such a method exist in the controller ?
-            if (method_exists($this->url_controller, $this->url_action)) {
-
-                // call the method and pass the arguments to it
-                if (isset($this->url_parameter_3)) {
-                    // will translate to something like $this->home->method($param_1, $param_2, $param_3);
-                    $this->url_controller->{$this->url_action}($this->url_parameter_1, $this->url_parameter_2, $this->url_parameter_3);
-                } elseif (isset($this->url_parameter_2)) {
-                    // will translate to something like $this->home->method($param_1, $param_2);
-                    $this->url_controller->{$this->url_action}($this->url_parameter_1, $this->url_parameter_2);
-                } elseif (isset($this->url_parameter_1)) {
-                    // will translate to something like $this->home->method($param_1);
-                    $this->url_controller->{$this->url_action}($this->url_parameter_1);
+            if (method_exists($this->url_controller, $this->url_action)) { // check for method exist?
+                if(!empty($this->url_params)) { // Call the method and pass arguments to it                    
+                    call_user_func_array(array($this->url_controller, $this->url_action), $this->url_params);
                 } else {
-                    // if no parameters given, just call the method without parameters, like $this->home->method();
-                    $this->url_controller->{$this->url_action}();
+                    $this->url_controller->{$this->url_action}(); // No parameters given? call without parameter
                 }
             } else {
-                // default/fallback: call the index() method of a selected controller
-                $this->url_controller->index();
+                if(strlen($this->url_action) == 0) {
+                    $this->url_controller->index(); // no action? call the default index() method of a selected controller
+                }
+                else {
+                    require './application/controller/errors.php'; // defined action not existent: show the error page
+                    $pg = new Errors();
+                    $pg->index();
+                }
             }
+        // here we redirect to errors controller if no matched controller name
         } else {
-            /* invalid URL, so simply redirect to home/index*/
-            require './application/controller/home.php';
-            $home = new Home();
-            $home->index();
+            require './application/controller/errors.php';
+            $pg = new Errors();
+            $pg->index();
         }
     }
 
-    /**
-     * Get and split the URL
-     */
-    private function splitUrl() {
-        // check url
-        if (isset($_GET['url'])) {
-            // split URL
-            $url = rtrim($_GET['url'], '/');
+    /** Get and split the URL */
+    private function splitUrl()
+    {
+        if (isset($_GET['url'])) {   // split URL
+            $url = trim($_GET['url'], '/');
             $url = filter_var($url, FILTER_SANITIZE_URL);
             $url = explode('/', $url);
-
             // Put URL parts into according properties
-            // By the way, the syntax here is just a short form of if/else, called "Ternary Operators"
-
-            $this->url_controller = (isset($url[0]) ? $url[0] : null);
-            $this->url_action = (isset($url[1]) ? $url[1] : null);
-            $this->url_parameter_1 = (isset($url[2]) ? $url[2] : null);
-            $this->url_parameter_2 = (isset($url[3]) ? $url[3] : null);
-            $this->url_parameter_3 = (isset($url[4]) ? $url[4] : null);
-
+            $this->url_controller = isset($url[0]) ? $url[0] : null; 
+            $this->url_action = isset($url[1]) ? $url[1] : null;
+            // Remove controller and action from split URL
+            unset($url[0], $url[1]); 
+            // Rebase array keys and store the URL params
+            $this->url_params = array_values($url); 
         }
     }
 }
