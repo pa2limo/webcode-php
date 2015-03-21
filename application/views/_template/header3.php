5ea25aa82f946019f6f44a2cd3405022f8e0fb65
Muhammad Pudhail <pa2limo@users.noreply.github.com>
2015-03-21 14:33:04 +0700

    Create header3.php

---
+++ application/views/_template/header3.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>WEBCODE-<?php echo ($blogs->blog_title); ?></title>
+	<meta name="viewport"    content="width=device-width, initial-scale=1.0">
+
+	<meta name="description" content="PHP 5 Best Practices, PHP 5 Coding Guide, PHP 5 Security, PHP 5 OOP, PHP PDO MySQL, PHP and Bootstrap, Web Template PHP">
+	<meta name="author"      content="Muhammad Pudhail">
+
+	<!--<meta http-equiv="refresh" content="10">  refresh 10s -->
+	<!--<meta http-equiv="refresh" content="10;url=http://redirect-url.com">  redirect after 10s -->
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,700,900,400italic">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
+    <!-- highlightjs.org/download/ 
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.3/styles/default.min.css"> -->
+    <link rel="stylesheet" href="<?php echo ASSET; ?>css/shsyntaxhighlater.css">
+	<link href="<?php echo ASSET; ?>css/style.css" rel="stylesheet" />
+	<!-- Theme skin -->
+	<link href="<?php echo ASSET; ?>css/default.css" rel="stylesheet" />
+
+	<style type="text/css">
+	</style> 
+	<!-- css -->
+	<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif] -->
+</head>
