b36a871a3371c1596c3636d9c58ad38dcbe7a9c5
Muhammad Pudhail <pa2limo@users.noreply.github.com>
2015-03-21 14:35:13 +0700

    Update index.php

--- application/views/blogs/index.php
+++ application/views/blogs/index.php
@@ -1,4 +1,3 @@
-/* This blogs/index.php */
 <body>
 <div id="wrapper">
 	<!-- start header -->
@@ -12,16 +11,16 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="<?php echo URL; ?>/home/index.php"><span>WebCode</span>/BLOG</a>
+                    <a class="navbar-brand" href="<?php echo URL; ?>"><span>WebCode</span>/BLOG</a>
                 </div>
 
                 <div class="navbar-collapse collapse ">
                     <ul class="nav navbar-nav">
-                        <li><a href="<?php echo URL; ?>/home/index.php" target="_blank"><h4>Home</h4></a></li>
-                        <li class="active"><a href="<?php echo URL; ?>/blogs/index.php"><h4>Blogs</h4></a></li>
-                        <li><a href="<?php echo URL; ?>/snippets/index.php" target="_blank"><h4>Snippet</h4></a></li>
-                        <li><a href="<?php echo URL; ?>/tutors/index.php" target="_blank"><h4>Tutorial</h4></a></li>
-                        <li><a href="<?php echo URL; ?>/webtemplates/index.php" target="_blank"><h4>Template</h4></a></li>
+                        <li><a href="<?php echo URL; ?>"><h4>Home</h4></a></li>
+                        <li class="active"><a href="<?php echo URL; ?>blogs/"><h4>Blogs</h4></a></li>
+                        <li><a href="<?php echo URL; ?>snippets/" target="_blank"><h4>Snippet</h4></a></li>
+                        <li><a href="<?php echo URL; ?>tutorials/" target="_blank"><h4>Tutorial</h4></a></li>
+                        <li><a href="<?php echo URL; ?>webtemplates/" target="_blank"><h4>Template</h4></a></li>
                     </ul>
                 </div>
 
@@ -45,8 +44,23 @@
 	<!-- Start Main Content -->
 	<section id="content">
 	<div class="container">
-		<div class="row"><img src="<?php echo ASSET ; ?>img/blog/headerblog1.jpg" alt="" />
-		
+		<div class="row">
+			<?php
+					srand( microtime() * 10000 );
+					  $num = rand( 1, 4 );					   
+					  switch( $num )  {
+					  	  case 1: $images = ASSET.'img/blog/headerblog1.jpg';
+						          break;
+						  case 2: $images = ASSET.'img/blog/headerblog11.jpg';
+						          break;
+						  case 3: $images = ASSET.'img/blog/headerblog2.jpg';
+						          break;
+						  case 4: $images = ASSET.'img/blog/headerblog10.jpg';
+						          break;
+					  }
+					  //echo "Random Image : <img src=$image_file />"; 
+					?>
+			<img src="<?php echo ($images);?>" alt="" />
 			<div class="col-lg-8">	
 
 			<?php    
@@ -59,7 +73,7 @@
 				if (isset($pag->blog_title)) $slug=create_slug($pag->blog_title);
 				if (isset($pag->blog_id)) $id=$pag->blog_id;					
 				if (isset($pag->blog_img)) $img=$pag->blog_img;	
-				if (isset($pag->cat_name)) $cname=$pag->cat_name;	
+				// if (isset($pag->cat_name)) $cname=$pag->cat_name;	
 				++$index;
 				?> 
 					<article>  
@@ -81,7 +95,7 @@
 							    <li><span class="glyphicon glyphicon-eye-open"></span><?php if (isset($pag->blog_read)) echo " ",($pag->blog_read)," Read"; ?></li>
 								<li><span class="glyphicon glyphicon-comment"></span><?php if (isset($pag->comment_count)) echo " ",($pag->comment_count)," "; ?> Comments</li>
 							</ul>
-							<a href="<?php echo URL.'/blogs/show/'.$id.'/'.$slug ; ?>" class="pull-right"><strong>READ MORE</strong> </a>
+							<a href="<?php echo URL.'blogs/show/'.$id.'/'.$slug ; ?>" class="pull-right"><strong>READ MORE</strong> </a>
 						</div>
 					</article> 
 
@@ -122,64 +136,46 @@
 						<input class="form-control" type="text" placeholder="Search..">
 					</form>
 				</div>
+
 				<div class="widget">
 					<h5 class="widgetheading">Categories</h5>
-					<ul class="cat">
+					<ul class="tags">
 						<?php 
 						foreach ($category as $cats)	
 						{  
 							if (isset($cats->cat_id)) $cid=$cats->cat_id;
-							if (isset($cats->cat_name)) $cname=$cats->cat_name; 
+							if (isset($cats->cat_name)) $cname=create_slug($cats->cat_name); 
 							?>
-							<li><a href="<?php echo URL.'/blogs/show_cat/'.$cid.'/'.$cname ; ?>"><span>
-							<?php  echo ($cname)," (", ($cats->blog_count),")" ; ?> </span></a></li><?php
+							<li><a href="<?php echo URL.'blogs/show_cat/'.$cid.'/'.$cname ; ?>"><span>
+							<?php  echo ($cname),' (', ($cats->blog_count),')' ; ?> </span></a></li><?php
 					    } ?>
-					</ul>
-					
-				</div>
-				<div class="widget">
-					<h5 class="widgetheading">Latest posts</h5>
-					<ul class="recent">
-						<li>
-						<img src="<?php echo ASSET ; ?>img/dummies/blog/65x65/thumb1.jpg" class="pull-left" alt="" />
-						<h6><a href="#">Lorem ipsum dolor sit</a></h6>
-						<p>
-							 Mazim alienum appellantur eu cu ullum officiis pro pri
-						</p>
-						</li>
-						<li>
-						<a href="#"><img src="<?php echo ASSET ; ?>img/dummies/blog/65x65/thumb2.jpg" class="pull-left" alt="" /></a>
-						<h6><a href="#">Maiorum ponderum eum</a></h6>
-						<p>
-							 Mazim alienum appellantur eu cu ullum officiis pro pri
-						</p>
-						</li>
-						<li>
-						<a href="#"><img src="<?php echo ASSET ; ?>img/dummies/blog/65x65/thumb3.jpg" class="pull-left" alt="" /></a>
-						<h6><a href="#">Et mei iusto dolorum</a></h6>
-						<p>
-							 Mazim alienum appellantur eu cu ullum officiis pro pri
-						</p>
-						</li>
 					</ul>
 				</div>
 
+				
 				<div class="widget">
-					<h5 class="widgetheading">Popular tags</h5>
-					<ul class="tags">
-						<?php 
-						foreach ($tags as $tg)	
-						{  
-							if (isset($tg->tag_id)) $tid=$tg->tag_id;
-							if (isset($tg->tag_name)) $tname=create_slug($tg->tag_name); 
-							if ($tg->blog_count>0)
-								{ ?>
-							       <li><a href="<?php echo URL.'/blogs/show_tag/'.$tid.'/'.$tname ; ?>">
-							       <span><?php echo ($tname)," (", ($tg->blog_count),")" ; ?>
-							 	   </span></a></li>
-						<?php 	}
-					    } 
-					    ?> 
+					<h5 class="widgetheading">Most Read</h5>
+					<ul class="recent">
+					<?php 
+					$gbr=1;
+					foreach ($pops as $pop)	
+					{  	
+				        if (isset($pop->blog_img)) $img=$pop->blog_img;	
+				        if (isset($pop->cat_name)) $cname=$pop->cat_name;
+				        if (isset($pop->blog_id)) $id=$pop->blog_id; ?>			
+							<li>   
+							<img src="<?php echo ASSET ; ?>img/dummies/blog/65x65/thumb<?php echo $gbr ; ?>.jpg" class="pull-left" alt="" />
+							<?php
+							if (isset($pop->blog_title)) {
+								$slug=create_slug($pop->blog_title);
+							} ?>
+						
+						<h6><a href="<?php echo URL.'blogs/show/'.$id.'/'.$slug ; ?>">   
+						     <?php echo get_words($pop->blog_title, 4); ?></a>
+						</h6>
+						<p><?php if (isset($pop->blog_content)) echo get_words($pop->blog_content, 11), "</p>";	
+						$gbr++;
+					}?>
 					</ul>
 				</div>
 
@@ -193,6 +189,23 @@
 	            	</ul>
 				</div>
 
+				<div class="widget">
+					<?php
+					srand( microtime() * 8000 );
+					  $num = rand( 1, 4 );					   
+					  switch( $num )  {
+					  	  case 1: $image_file = ASSET.'img/blog/widget2.jpg';
+						          break;
+						  case 2: $image_file = ASSET.'img/blog/widget3.jpg';
+						          break;
+						  case 3: $image_file = ASSET.'img/blog/widget4.jpg';
+						          break;
+						  case 4: $image_file = ASSET.'img/blog/widget5.jpg';
+						          break;
+					  }	?>
+					<p><img src="<?php echo ($image_file) ; ?>" class="pull-left" alt="" /></p>
+	            </div>
+	            
 				</aside>
 			</div>
 		</div>
