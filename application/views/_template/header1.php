ec5d9fa8c887722b6e04251ced950c3f54267031
Muhammad Pudhail <pa2limo@users.noreply.github.com>
2015-03-21 14:29:32 +0700

    Create header1.php

---
+++ application/views/_template/header1.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+	<title>WEBCODE - Learning Resources for Web Coding With PHP 5</title>
+	<meta name="viewport"    content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Blog about PHP 5, PHP Code, PHP Snippet, PHP Best Practices, PHP PDO, Bootstrap, Web Template">
+	<meta name="author"      content="Muhammad Pudhail">
+	<meta name="robots" 	 content="index">
+    <!-- <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,500"> -->
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,600,700,400italic"> 
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:100,300,400,600,700,900,400italic"> 
+    <!-- css 
+	<link href="<?php //echo ASSET; ?>css/bootstrap.min.css" rel="stylesheet" />-->
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+ 	<link href="<?php echo ASSET; ?>css/flexslider.css" rel="stylesheet">
+	<link href="<?php echo ASSET; ?>css/style.css" rel="stylesheet">
+	<!-- Theme skin -->
+	<link href="<?php echo ASSET; ?>css/default.css" rel="stylesheet">
+	<!-- Place your own css here -->
+	<style type="text/css">
+	.thumbnail {position: relative;padding: 0px;font-size: 12px;margin-bottom: 20px;}
+	.thumbnail img {width: 100%;}
+	a {
+	  color: #e1ffff;
+	  text-decoration: none;
+	}
+	a:hover,
+	a:focus {
+	  color: #fffcb2;
+	  text-decoration: underline;
+	}
+	</style> 
+
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="<?php echo ASSET; ?>js/html5shiv.js"></script>
+	<script src="<?php echo ASSET; ?>js/respond.min.js"></script>
+	<![endif]-->
+
+</head>	
