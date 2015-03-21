ca797c79c41741c48342d3e085f945117ce10dae
Muhammad Pudhail <pa2limo@users.noreply.github.com>
2015-03-21 13:58:39 +0700

    Create errors.php

---
+++ application/controller/errors.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Class Home
+ *
+ * Please note:
+ * Don't use the same name for class and method, as this might trigger an (unintended) __construct of the class.
+ * This is really weird behaviour, but documented here: http://php.net/manual/en/language.oop5.decon.php
+ *
+ */
+class Errors extends Controller
+{
+    /**
+     * PAGE: index
+     * This method handles what happens when you move to http://yourproject/home/index (which is the default page btw)
+     */
+    public function index()
+    {
+        // debug message to show where you are, just for the demo
+        
+        // load views. within the views we can echo out $songs and $amount_of_songs easily
+        require 'application/views/error/index.php';
+        
+    }  
+}
